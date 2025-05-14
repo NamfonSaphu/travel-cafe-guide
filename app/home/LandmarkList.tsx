c99947316f7eb4cd8dfb2510a95e61f73182a032
NamfonSaphu <namfonsaphu@gmail.com>
2025-05-15 01:10:56 +0700

    loading page

--- app/home/LandmarkList.tsx
+++ app/home/LandmarkList.tsx
@@ -3,7 +3,7 @@ import { LandmarkCardProps } from "@/utils/types"
 
 const LandmarkList = ({ landmarks }: { landmarks: LandmarkCardProps[] }) => {
     return (
-        <section className="grid sm:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-8 mt-4">
+        <section className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-4">
             {
                 landmarks.map((landmark) => {
                     return <LandmarkCard landmark={landmark} key={landmark.id} />
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import LandmarkContainer from "./home/LandmarkContainer";
+import LoadingCard from "@/components/card/LoadingCard";
 
 export default function Home() {
   return (
-    <>
-      <LandmarkContainer />
-    </>
+    <section>
+      <Suspense fallback={<LoadingCard />}>
+        <LandmarkContainer />
+      </Suspense>
+    </section>
   );
 }
