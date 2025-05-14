ed291aa2aef75561be9ba0055bee901fc1a59995
NamfonSaphu <namfonsaphu@gmail.com>
2025-05-14 22:39:40 +0700

    add signin button fav

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,11 @@
 import { LuHeart } from "react-icons/lu";
 import { Button } from "../ui/button";
+import { auth } from '@clerk/nextjs/server'
+import { SignInCardButton } from "../form/Buttons";
 
-const FavoriteToggleButton = ({ landmarkId }: { landmarkId: string }) => {
+const FavoriteToggleButton = async ({ landmarkId }: { landmarkId: string }) => {
+    const { userId } = await auth()
+    if (!userId) return <SignInCardButton />
     return (
         <Button size='icon'>
             <LuHeart />
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -2,6 +2,8 @@
 import { useFormStatus } from "react-dom"
 import { Button } from "../ui/button"
 import { LoaderCircle } from 'lucide-react';
+import { SignInButton } from "@clerk/nextjs";
+import { LuHeart } from "react-icons/lu";
 
 type btnSize = 'default' | 'lg' | 'sm'
 
@@ -18,12 +20,23 @@ export const SubmitButton = ({ className, size, text }: SubmitButtonProps) => {
         <Button disabled={pending} className={className} size={size} type="submit" >
             {
                 pending
-                    ?<>
-                    <LoaderCircle className="animate-spin" />
-                    <span>Please wait...</span>
-                    </> 
+                    ? <>
+                        <LoaderCircle className="animate-spin" />
+                        <span>Please wait...</span>
+                    </>
                     : <p>{text}</p>
             }
         </Button>
+    )
+}
+
+export const SignInCardButton = () => {
+    return (
+        <SignInButton mode='modal'>
+            <Button size='icon'>
+                <LuHeart />
+            </Button>
+        </SignInButton>
+
     )
 }
