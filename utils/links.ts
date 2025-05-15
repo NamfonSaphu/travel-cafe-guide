c1ce6b4ae8ebf237ae29366b36393022037dd25c
NamfonSaphu <namfonsaphu@gmail.com>
2025-05-15 21:26:55 +0700

    edit links

--- utils/links.ts
+++ utils/links.ts
@@ -5,8 +5,6 @@ type NavLinks = {
 
 export const links: NavLinks[] = [
     { href: '/', label: 'Home' },
-    { href: '/profile', label: 'Profile' },
     { href: '/favorites', label: 'Favorite' },
-    { href: '/tourist', label: 'Tourist' }, 
     { href: '/tourist/create', label: 'Create Landmark' }, 
 ]
